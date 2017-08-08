75ce94de7d7c191dbba2301fa2c2faccd1584ac5
Lexis von Seggern <lexvon2015@gmail.com>
2017-08-08 13:56:28 -0500

    adding erb's

--- main.rb
+++ main.rb
@@ -2,9 +2,16 @@ require "sinatra"
 require_relative "./csv.rb"
 
 get '/myhomepage' do
-	account = run_csv_processer 
+	
 	erb(:main)
 end
 
+get '/full' do
+	account = run_csv_processer 
+	erb(:full)
+end
 
-
+get '/account' do
+	account = run_csv_processer
+	erb(:account)
+end
