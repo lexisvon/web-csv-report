a2e35fd9b0873b431f774eb4631e7c4a79be53c2
Sumeet Jain <sumeet@sumeetjain.com>
2017-08-10 11:27:00 -0500

    Pairing w/ Sumeet
    Got started with adding rows to a file using Ruby.
    
    Also began the form for taking user input.

--- main.rb
+++ main.rb
@@ -15,3 +15,32 @@ get '/full' do
 	accounts = run_csv_processer
 	erb(:full, :locals => {:accounts => accounts})
 end
+
+# Showing the form that they type new row values into.
+get '/add_row_form' do
+  erb(:add_row_form)
+end
+
+# Processes the form that they typed new row values into.
+get '/add_row' do
+  name = "Priya"
+  date = "1/1/2017"
+  payee = "BOB'S BARBER SHOP"
+  category = "Allowance"
+  inflow = "$0.00"
+  outflow = "$3,800.00"
+
+  open('temp.txt', 'a') { |file|
+    str = ""
+    str << name + ","
+    str << date + ","
+    str << payee + ","
+    str << category + ","
+    str << inflow + ","
+    str << outflow + "\n"
+
+    file << str
+  }
+
+  erb(:success)
+end
